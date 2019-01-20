ab3d31376959d3607de12cbeec7172d11d1092a9
Taras <senivtar1999@gmail.com>
2019-01-20 22:42:12 +0200

    make code format

--- js/app.js
+++ js/app.js
@@ -1,74 +1,73 @@
 let toolTip = document.querySelector('.tooltip[data-switch="true"]');
 
 function getToolTipContent(toolTip) {
-    if(!toolTip) {
-        return;
-    }
+  if (!toolTip) {
+    return;
+  }
 
-    let toolTipText = toolTip.querySelector('.tooltip-content');
+  let toolTipText = toolTip.querySelector(".tooltip-content");
 
-    return {
-        delay: parseInt(toolTip.dataset.delay),
-        text: toolTip.dataset.value
-    };
+  return {
+    delay: parseInt(toolTip.dataset.delay),
+    text: toolTip.dataset.value
+  };
 }
 
 function updateToolTip(e, tooltipText) {
-    if(!toolTip) {
-        return;
-    }
+  if (!toolTip) {
+    return;
+  }
 
-    if(!toolTip.classList.contains('tooltip-active')) {
-        toolTip.classList.add('tooltip-active');
-    }
+  if (!toolTip.classList.contains("tooltip-active")) {
+    toolTip.classList.add("tooltip-active");
+  }
 
-    let sizeOfPointer = 10;
-    let toolTipWidth = toolTip.clientWidth;
-    let toolTipHeight = toolTip.clientHeight;
+  let sizeOfPointer = 10;
+  let toolTipWidth = toolTip.clientWidth;
+  let toolTipHeight = toolTip.clientHeight;
 
-    let tollTipCenterX = e.clientX - toolTipWidth / 2;
-    tollTipCenterX = (tollTipCenterX < 0) ? 0 : tollTipCenterX;
+  let tollTipCenterX = e.clientX - toolTipWidth / 2;
+  tollTipCenterX = tollTipCenterX < 0 ? 0 : tollTipCenterX;
 
-    let tollTipCenterY = e.clientY - toolTipHeight - sizeOfPointer;
-    tollTipCenterY = (tollTipCenterY < 0) ? 0 : tollTipCenterY;
+  let tollTipCenterY = e.clientY - toolTipHeight - sizeOfPointer;
+  tollTipCenterY = tollTipCenterY < 0 ? 0 : tollTipCenterY;
 
-    toolTip.style.left = `${tollTipCenterX}px`;
-    toolTip.style.top = `${tollTipCenterY}px`;
+  toolTip.style.left = `${tollTipCenterX}px`;
+  toolTip.style.top = `${tollTipCenterY}px`;
 
-    if(tooltipText) {
-        toolTip.firstElementChild.textContent = tooltipText + ' ';
-    }
+  if (tooltipText) {
+    toolTip.firstElementChild.textContent = tooltipText + " ";
+  }
 
-    toolTip.firstElementChild.textContent += `x: ${e.clientX} y: ${e.clientY}`;
+  toolTip.firstElementChild.textContent += `x: ${e.clientX} y: ${e.clientY}`;
 }
 
 function throttle(func, delay) {
-    let timerId;
-    let prevTime = 0;
-    let params = getToolTipContent(toolTip);
+  let timerId;
+  let prevTime = 0;
+  let params = getToolTipContent(toolTip);
 
-    let newDeley = params.delay;
-    let newToolTipTextContent = params.text;
-    delay = newDeley || delay;
+  let newDeley = params.delay;
+  let newToolTipTextContent = params.text;
+  delay = newDeley || delay;
 
-    return function (...args) {
-        let currTime = new Date();
+  return function(...args) {
+    let currTime = new Date();
 
-        if(timerId) {
-            clearTimeout(timerId);
-        }
+    if (timerId) {
+      clearTimeout(timerId);
+    }
 
-        if(currTime - prevTime >= delay) {
-            updateToolTip.call(this, ...args, newToolTipTextContent);
-            prevTime = currTime;
-        }
+    if (currTime - prevTime >= delay) {
+      updateToolTip.call(this, ...args, newToolTipTextContent);
+      prevTime = currTime;
+    }
 
-        timerId = setTimeout(() => {
-            updateToolTip.call(this, ...args, newToolTipTextContent)
-            prevTime = 0;
-        }, delay);
-
-    };
+    timerId = setTimeout(() => {
+      updateToolTip.call(this, ...args, newToolTipTextContent);
+      prevTime = 0;
+    }, delay);
+  };
 }
 
 let wrapper = throttle(updateToolTip, 500);
